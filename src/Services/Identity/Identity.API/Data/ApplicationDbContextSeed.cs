b919a59eba73f62cbd451fc937b989c28e182e2e
Dobromir Zlatkov <dobromir@twogears.com>
2018-01-02 21:29:38 +0200

    Configure db context.

---
+++ src/Services/Identity/Identity.API/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,102 @@
+﻿using Identity.API.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API.Data
+{
+
+    public class ApplicationDbContextSeed
+    {
+        private readonly IPasswordHasher<ApplicationUser> _passwordHasher = new PasswordHasher<ApplicationUser>();
+
+        public async Task SeedAsync(ApplicationDbContext context, IHostingEnvironment env,
+            ILogger<ApplicationDbContextSeed> logger, IOptions<AppSettings> settings, int? retry = 0)
+        {
+            int retryForAvaiability = retry.Value;
+
+            try
+            {
+                var useCustomizationData = settings.Value.UseCustomizationData;
+                var contentRootPath = env.ContentRootPath;
+                var webroot = env.WebRootPath;
+
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(GetDefaultUser());
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                if (retryForAvaiability < 10)
+                {
+                    retryForAvaiability++;
+
+                    logger.LogError(ex.Message, $"There is an error migrating data for ApplicationDbContext");
+
+                    await SeedAsync(context, env, logger, settings, retryForAvaiability);
+                }
+            }
+        }
+
+        private ApplicationUser CreateApplicationUser(string[] column, string[] headers)
+        {
+            if (column.Count() != headers.Count())
+            {
+                throw new Exception($"column count '{column.Count()}' not the same as headers count'{headers.Count()}'");
+            }
+
+            string cardtypeString = column[Array.IndexOf(headers, "cardtype")].Trim('"').Trim();
+            if (!int.TryParse(cardtypeString, out int cardtype))
+            {
+                throw new Exception($"cardtype='{cardtypeString}' is not a number");
+            }
+
+            var user = new ApplicationUser
+            {
+                Email = column[Array.IndexOf(headers, "email")].Trim('"').Trim(),
+                Id = Guid.NewGuid().ToString(),
+                PhoneNumber = column[Array.IndexOf(headers, "phonenumber")].Trim('"').Trim(),
+                UserName = column[Array.IndexOf(headers, "username")].Trim('"').Trim(),
+               
+                NormalizedEmail = column[Array.IndexOf(headers, "normalizedemail")].Trim('"').Trim(),
+                NormalizedUserName = column[Array.IndexOf(headers, "normalizedusername")].Trim('"').Trim(),
+                SecurityStamp = Guid.NewGuid().ToString("D"),
+                PasswordHash = column[Array.IndexOf(headers, "password")].Trim('"').Trim(), // Note: This is the password
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, user.PasswordHash);
+
+            return user;
+        }
+
+        private IEnumerable<ApplicationUser> GetDefaultUser()
+        {
+            var user =
+            new ApplicationUser()
+            {
+                Email = "demouser@microsoft.com",
+                Id = Guid.NewGuid().ToString(),
+                PhoneNumber = "1234567890",
+                UserName = "demouser@microsoft.com",
+                NormalizedEmail = "DEMOUSER@MICROSOFT.COM",
+                NormalizedUserName = "DEMOUSER@MICROSOFT.COM",
+                SecurityStamp = Guid.NewGuid().ToString("D"),
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, "Pass@word1");
+
+            return new List<ApplicationUser>()
+            {
+                user
+            };
+        }
+    }
+}
