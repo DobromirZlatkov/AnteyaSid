814d21d2001cc6cbbe702af240051c3d110d843b
DESKTOP-LB0GKAQ\dz <dobromir@twogears.com>
2018-01-15 19:54:02 +0200

    Create_Identity.API - Add initial migrations

--- src/Services/Identity/Identity.API/Migrations/20180115174446_InitialCreate.Designer.cs
+++ src/Services/Identity/Identity.API/Migrations/20180115174446_InitialCreate.Designer.cs
@@ -8,12 +8,13 @@ using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 
-namespace AnteyaSidOnContainers.Services.Identity.API
+namespace AnteyaSidOnContainers.Services.Identity.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180115174446_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
