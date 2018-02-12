3148f11f0f88138f3df2bf65cbec70542f5999d8
DESKTOP-LB0GKAQ\dz <dobromir@twogears.com>
2018-02-12 17:55:24 +0200

    WebMVC - create project to consume identity api.

---
+++ src/Web Apps/WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnteyaSidOnContainers.WebApps.WebMVC.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
