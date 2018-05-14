3c9565323451185a9f939f20f82fb03c46082663
DESKTOP-LB0GKAQ\dz <dobromir@twogears.com>
2018-05-14 15:03:21 +0300

    Remove unwanted configuration.

--- src/Services/Identity/Identity.API/Configuration/Config.cs
+++ src/Services/Identity/Identity.API/Configuration/Config.cs
@@ -44,59 +44,60 @@ namespace AnteyaSidOnContainers.Services.Identity.API.Configuration
             return new List<Client>
             {
                  //JavaScript Client
-                new Client
-                {
-                    ClientId = "js",
-                    ClientName = "eShop SPA OpenId Client",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
-                    RedirectUris =           { $"{clientsUrl["Spa"]}/" },
-                    RequireConsent = false,
-                    PostLogoutRedirectUris = { $"{clientsUrl["Spa"]}/" },
-                    AllowedCorsOrigins =     { $"{clientsUrl["Spa"]}" },
-                    AllowedScopes =
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        "orders",
-                        "basket",
-                        "locations",
-                        "marketing"
-                    }
-                },
-                new Client
-                {
-                    ClientId = "xamarin",
-                    ClientName = "eShop Xamarin OpenId Client",
-                    AllowedGrantTypes = GrantTypes.Hybrid,                    
-                    //Used to retrieve the access token on the back channel.
-                    ClientSecrets =
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    RedirectUris = { clientsUrl["Xamarin"] },
-                    RequireConsent = false,
-                    RequirePkce = true,
-                    PostLogoutRedirectUris = { $"{clientsUrl["Xamarin"]}/Account/Redirecting" },
-                    AllowedCorsOrigins = { "http://eshopxamarin" },
-                    AllowedScopes = new List<string>
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        IdentityServerConstants.StandardScopes.OfflineAccess,
-                        "orders",
-                        "basket",
-                        "locations",
-                        "marketing"
-                    },
-                    //Allow requesting refresh tokens for long lived API access
-                    AllowOfflineAccess = true,
-                    AllowAccessTokensViaBrowser = true
-                },
+                //new Client
+                //{
+                //    ClientId = "js",
+                //    ClientName = "eShop SPA OpenId Client",
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+                //    AllowAccessTokensViaBrowser = true,
+                //    RedirectUris =           { $"{clientsUrl["Spa"]}/" },
+                //    RequireConsent = false,
+                //    PostLogoutRedirectUris = { $"{clientsUrl["Spa"]}/" },
+                //    AllowedCorsOrigins =     { $"{clientsUrl["Spa"]}" },
+                //    AllowedScopes =
+                //    {
+                //        IdentityServerConstants.StandardScopes.OpenId,
+                //        IdentityServerConstants.StandardScopes.Profile,
+                //        "orders",
+                //        "basket",
+                //        "locations",
+                //        "marketing"
+                //    }
+                //},
+                //new Client
+                //{
+                //    ClientId = "xamarin",
+                //    ClientName = "eShop Xamarin OpenId Client",
+                //    AllowedGrantTypes = GrantTypes.Hybrid,                    
+                //    //Used to retrieve the access token on the back channel.
+                //    ClientSecrets =
+                //    {
+                //        new Secret("secret".Sha256())
+                //    },
+                //    RedirectUris = { clientsUrl["Xamarin"] },
+                //    RequireConsent = false,
+                //    RequirePkce = true,
+                //    PostLogoutRedirectUris = { $"{clientsUrl["Xamarin"]}/Account/Redirecting" },
+                //    AllowedCorsOrigins = { "http://eshopxamarin" },
+                //    AllowedScopes = new List<string>
+                //    {
+                //        IdentityServerConstants.StandardScopes.OpenId,
+                //        IdentityServerConstants.StandardScopes.Profile,
+                //        IdentityServerConstants.StandardScopes.OfflineAccess,
+                //        "orders",
+                //        "basket",
+                //        "locations",
+                //        "marketing"
+                //    },
+                //    //Allow requesting refresh tokens for long lived API access
+                //    AllowOfflineAccess = true,
+                //    AllowAccessTokensViaBrowser = true
+                //},
                 new Client
                 {
                     ClientId = "mvc",
                     ClientName = "MVC Client",
+                    AccessTokenType = AccessTokenType.Reference,
                     ClientSecrets = new List<Secret>
                     {
                         new Secret("secret".Sha256())
@@ -105,7 +106,7 @@ namespace AnteyaSidOnContainers.Services.Identity.API.Configuration
                     AllowedGrantTypes = GrantTypes.Hybrid,
                     AllowAccessTokensViaBrowser = false,
                     RequireConsent = false,
-                    AllowOfflineAccess = true,
+                    AllowOfflineAccess = false,
                     AlwaysIncludeUserClaimsInIdToken = true,
                     RedirectUris = new List<string>
                     {
@@ -120,104 +121,104 @@ namespace AnteyaSidOnContainers.Services.Identity.API.Configuration
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
                         IdentityServerConstants.StandardScopes.OfflineAccess,
-                        "orders",
-                        "basket",
-                        "locations",
-                        "marketing"
+                        //"orders",
+                        //"basket",
+                        //"locations",
+                        //"marketing"
                     },
                 },
-                new Client
-                {
-                    ClientId = "mvctest",
-                    ClientName = "MVC Client Test",
-                    ClientSecrets = new List<Secret>
-                    {
-                        new Secret("secret".Sha256())
-                    },
-                    ClientUri = $"{clientsUrl["Mvc"]}",                             // public uri of the client
-                    AllowedGrantTypes = GrantTypes.Hybrid,
-                    AllowAccessTokensViaBrowser = true,
-                    RequireConsent = false,
-                    AllowOfflineAccess = true,
-                    RedirectUris = new List<string>
-                    {
-                        $"{clientsUrl["Mvc"]}/signin-oidc"
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        $"{clientsUrl["Mvc"]}/signout-callback-oidc"
-                    },
-                    AllowedScopes = new List<string>
-                    {
-                        IdentityServerConstants.StandardScopes.OpenId,
-                        IdentityServerConstants.StandardScopes.Profile,
-                        IdentityServerConstants.StandardScopes.OfflineAccess,
-                        "orders",
-                        "basket",
-                        "locations",
-                        "marketing"
-                    },
-                },
-                new Client
-                {
-                    ClientId = "locationsswaggerui",
-                    ClientName = "Locations Swagger UI",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
+                //new Client
+                //{
+                //    ClientId = "mvctest",
+                //    ClientName = "MVC Client Test",
+                //    ClientSecrets = new List<Secret>
+                //    {
+                //        new Secret("secret".Sha256())
+                //    },
+                //    ClientUri = $"{clientsUrl["Mvc"]}",                             // public uri of the client
+                //    AllowedGrantTypes = GrantTypes.Hybrid,
+                //    AllowAccessTokensViaBrowser = true,
+                //    RequireConsent = false,
+                //    AllowOfflineAccess = true,
+                //    RedirectUris = new List<string>
+                //    {
+                //        $"{clientsUrl["Mvc"]}/signin-oidc"
+                //    },
+                //    PostLogoutRedirectUris = new List<string>
+                //    {
+                //        $"{clientsUrl["Mvc"]}/signout-callback-oidc"
+                //    },
+                //    AllowedScopes = new List<string>
+                //    {
+                //        IdentityServerConstants.StandardScopes.OpenId,
+                //        IdentityServerConstants.StandardScopes.Profile,
+                //        IdentityServerConstants.StandardScopes.OfflineAccess,
+                //        "orders",
+                //        "basket",
+                //        "locations",
+                //        "marketing"
+                //    },
+                //},
+                //new Client
+                //{
+                //    ClientId = "locationsswaggerui",
+                //    ClientName = "Locations Swagger UI",
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+                //    AllowAccessTokensViaBrowser = true,
 
-                    RedirectUris = { $"{clientsUrl["LocationsApi"]}/swagger/o2c.html" },
-                    PostLogoutRedirectUris = { $"{clientsUrl["LocationsApi"]}/swagger/" },
+                //    RedirectUris = { $"{clientsUrl["LocationsApi"]}/swagger/o2c.html" },
+                //    PostLogoutRedirectUris = { $"{clientsUrl["LocationsApi"]}/swagger/" },
 
-                    AllowedScopes =
-                    {
-                        "locations"
-                    }
-                },
-                new Client
-                {
-                    ClientId = "marketingswaggerui",
-                    ClientName = "Marketing Swagger UI",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
+                //    AllowedScopes =
+                //    {
+                //        "locations"
+                //    }
+                //},
+                //new Client
+                //{
+                //    ClientId = "marketingswaggerui",
+                //    ClientName = "Marketing Swagger UI",
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+                //    AllowAccessTokensViaBrowser = true,
 
-                    RedirectUris = { $"{clientsUrl["MarketingApi"]}/swagger/o2c.html" },
-                    PostLogoutRedirectUris = { $"{clientsUrl["MarketingApi"]}/swagger/" },
+                //    RedirectUris = { $"{clientsUrl["MarketingApi"]}/swagger/o2c.html" },
+                //    PostLogoutRedirectUris = { $"{clientsUrl["MarketingApi"]}/swagger/" },
 
-                    AllowedScopes =
-                    {
-                        "marketing"
-                    }
-                },
-                new Client
-                {
-                    ClientId = "basketswaggerui",
-                    ClientName = "Basket Swagger UI",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
+                //    AllowedScopes =
+                //    {
+                //        "marketing"
+                //    }
+                //},
+                //new Client
+                //{
+                //    ClientId = "basketswaggerui",
+                //    ClientName = "Basket Swagger UI",
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+                //    AllowAccessTokensViaBrowser = true,
 
-                    RedirectUris = { $"{clientsUrl["BasketApi"]}/swagger/o2c.html" },
-                    PostLogoutRedirectUris = { $"{clientsUrl["BasketApi"]}/swagger/" },
+                //    RedirectUris = { $"{clientsUrl["BasketApi"]}/swagger/o2c.html" },
+                //    PostLogoutRedirectUris = { $"{clientsUrl["BasketApi"]}/swagger/" },
 
-                    AllowedScopes =
-                    {
-                        "basket"
-                    }
-                },
-                new Client
-                {
-                    ClientId = "orderingswaggerui",
-                    ClientName = "Ordering Swagger UI",
-                    AllowedGrantTypes = GrantTypes.Implicit,
-                    AllowAccessTokensViaBrowser = true,
+                //    AllowedScopes =
+                //    {
+                //        "basket"
+                //    }
+                //},
+                //new Client
+                //{
+                //    ClientId = "orderingswaggerui",
+                //    ClientName = "Ordering Swagger UI",
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+                //    AllowAccessTokensViaBrowser = true,
 
-                    RedirectUris = { $"{clientsUrl["OrderingApi"]}/swagger/o2c.html" },
-                    PostLogoutRedirectUris = { $"{clientsUrl["OrderingApi"]}/swagger/" },
+                //    RedirectUris = { $"{clientsUrl["OrderingApi"]}/swagger/o2c.html" },
+                //    PostLogoutRedirectUris = { $"{clientsUrl["OrderingApi"]}/swagger/" },
 
-                    AllowedScopes =
-                    {
-                        "orders"
-                    }
-                }
+                //    AllowedScopes =
+                //    {
+                //        "orders"
+                //    }
+                //}
             };
         }
     }
