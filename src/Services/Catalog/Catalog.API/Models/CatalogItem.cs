b65fe94dac61b4ceaf3d9068da7a4e4a20cd3f57
Dobromir Zlatkov <dobromir@twogears.com>
2018-06-18 18:43:43 +0300

    Add  Select Extensions. Add Catalog item model.

---
+++ src/Services/Catalog/Catalog.API/Models/CatalogItem.cs
@@ -0,0 +1,11 @@
+﻿namespace AnteyaSidOnContainers.Services.Catalog.API.Models
+{
+    public class CatalogItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
