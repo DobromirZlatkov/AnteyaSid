3d364aed4dfc02296901d2465c37150d6ca6a9db
Dobromir Zlatkov <dobromir@twogears.com>
2018-08-27 15:29:17 +0300

    Catalog_API-Authorize api calls - Extract getting access token from session in separate service for Web Mvc.

--- src/WebApps/WebMVC/Services/CatalogService.cs
+++ src/WebApps/WebMVC/Services/CatalogService.cs
@@ -1,80 +1,67 @@
 ﻿namespace AnteyaSidOnContainers.WebApps.WebMVC.Services
 {
-    using System.Threading.Tasks;
-    
-    using Microsoft.Extensions.Logging;
-    using Microsoft.Extensions.Options;
-    
+    using AnteyaSidOnContainers.BuildingBlocks.Resilience.Http.Contracts;
     using AnteyaSidOnContainers.WebApps.WebMVC.Infrastructure;
     using AnteyaSidOnContainers.WebApps.WebMVC.Services.Contracts;
-    using AnteyaSidOnContainers.BuildingBlocks.Resilience.Http.Contracts;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using System;
+    using System.Threading.Tasks;
 
     public class CatalogService : ICatalogService, IRemoteCrudService
     {
         private readonly IOptionsSnapshot<AppSettings> _settings;
         private readonly IHttpClient _apiClient;
         private readonly ILogger<CatalogService> _logger;
-        private readonly IHttpContextAccessor _httpContextAccesor;
+        private readonly IAuthService _authService;
 
         private readonly string _remoteServiceBaseUrl;
 
         public CatalogService(
             IOptionsSnapshot<AppSettings> settings,
             IHttpClient httpClient,
-            IHttpContextAccessor httpContextAccesor,
+            IAuthService authService,
             ILogger<CatalogService> logger)
         {
             _settings = settings;
-            _apiClient = httpClient;
-            _httpContextAccesor = httpContextAccesor;
-            _logger = logger;
+            _apiClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _authService = authService ?? throw new ArgumentNullException(nameof(authService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
             _remoteServiceBaseUrl = $"{_settings.Value.CatalogUrl}/api/v1/catalog/";
         }
 
         public async Task<string> GetData(string queryParams)
         {
-            var token = await GetUserTokenAsync();
+            var token = await _authService.GetUserTokenAsync();
             var allcatalogItemsUri = API.Catalog.GetAllCatalogItems(_remoteServiceBaseUrl, queryParams);
             var dataString = await _apiClient.GetStringAsync(allcatalogItemsUri, token);
 
             return dataString;
         }
 
-
         public async Task<string> Create(object jsonObject)
         {
+            var token = await _authService.GetUserTokenAsync();
             var url = API.Catalog.CreateCatalogItem(_remoteServiceBaseUrl);
-
-            var response = await _apiClient.PostAsync(url, jsonObject);
-
+            var response = await _apiClient.PostAsync(url, jsonObject, token);
             return await response.Content.ReadAsStringAsync();
         }
 
         public async Task<string> Delete(int id)
         {
+            var token = await _authService.GetUserTokenAsync();
             var url = API.Catalog.DeleteCatalogItem(_remoteServiceBaseUrl, id);
-
-            var response = await _apiClient.PostAsync(url, id);
-
+            var response = await _apiClient.PostAsync(url, id, token);
             return await response.Content.ReadAsStringAsync();
         }
 
         public async Task<string> Update(object jsonObject)
         {
+            var token = await _authService.GetUserTokenAsync();
             var url = API.Catalog.UpdateCatalogItem(_remoteServiceBaseUrl);
-
-            var response = await _apiClient.PostAsync(url, jsonObject);
-
+            var response = await _apiClient.PostAsync(url, jsonObject, token);
             return await response.Content.ReadAsStringAsync();
-        }
-
-        async Task<string> GetUserTokenAsync()
-        {
-            var context = _httpContextAccesor.HttpContext;
-            return await context.GetTokenAsync("access_token");
         }
     }
 }
